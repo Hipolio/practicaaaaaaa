76155ba7c58aa8af8b972e6c408bf2c475614b76
Hipolio <115099287+Hipolio@users.noreply.github.com>
2025-09-10 17:03:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -218,13 +218,14 @@ def eliminarCargo():
     if not con.is_connected():
         con.reconnect()
     idCargo = request.form["idCargos"]
-    cursor = con.cursor(dictionary=True)
+    cursor = con.cursor()
     sql    = "DELETE FROM cargos WHERE idCargos = %s"
-    val    = (idCargo, )
+    val    = (idCargo)
     cursor.execute(sql, val)
     con.commit()
     con.close()
     return make_response(jsonify({"succes": True}))
+
 
 
 
